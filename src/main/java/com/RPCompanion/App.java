0de81752deaa7f32809c80341f7a407637d29d42
Ulises Justo Saucedo <ulisesjustosaucedo@gmail.com>
2024-01-31 17:32:12 -0300

    First state of the project

---
+++ src/main/java/com/RPCompanion/App.java
@@ -0,0 +1,6 @@
+package com.RPCompanion;
+public class App {
+    public static void main(String[] args) throws Exception{
+        System.out.print("Hello, RPCompanion!");
+    }
+}
---
+++ src/main/java/com/RPCompanion/Test.java
@@ -0,0 +1,5 @@
+package com.RPCompanion;
+import jakarta.persistence.Entity;
+@Entity
+public class Test {
+}
