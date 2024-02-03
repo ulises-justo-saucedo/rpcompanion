64565cdb7092af1ded47c2f012b69e8f1ff7d144
Ulises Justo Saucedo <ulisesjustosaucedo@gmail.com>
2024-02-03 08:44:31 -0300

    Removed unnecessary imports in App class

--- src/main/java/com/RPCompanion/App.java
+++ src/main/java/com/RPCompanion/App.java
@@ -1,19 +1,7 @@
 package com.RPCompanion;
 import com.RPCompanion.database.connection.DatabaseConnection;
-import com.RPCompanion.entities.GalleryEntity;
-import com.RPCompanion.entities.PowerEntity;
-import com.RPCompanion.entities.RPCharacterEntity;
 import com.RPCompanion.exceptions.DatabaseAccessException;
 import com.RPCompanion.exceptions.PropertiesFileException;
-import com.RPCompanion.services.GalleryService;
-import com.RPCompanion.services.PowerService;
-import com.RPCompanion.services.RPCharacterService;
-import javax.sql.rowset.serial.SerialBlob;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.sql.Date;
-import java.sql.SQLException;
 
 public class App {
     public static void main(String[] args) throws PropertiesFileException, DatabaseAccessException {
